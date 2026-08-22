cd36efaa9f7175b57cf958dbe9fb12f4a3094c6c
María Camila Moreno <124942762+mcmorenou@users.noreply.github.com>
2026-08-22 14:07:37 -0500

    complete exercice  01

---
+++ LogicConceots/IsNumerOddOrNot/Program.cs
@@ -0,0 +1,33 @@
+﻿var numberString = string.Empty;
+
+do
+{
+    Console.Write("Ingrese número entero o la palabra ´salir´ para salir: ");
+    numberString = Console.ReadLine();
+    if (numberString == "S" )
+    {
+        continue;
+
+    }
+    var numberInt = 0;
+    if (int.TryParse(numberString, out numberInt))
+    {
+        if (numberInt % 2 == 0)
+        {
+            Console.WriteLine($"El número {numberString}, es par.");
+        }
+        else
+        {
+            Console.WriteLine($"El número {numberString}, es imapar.");
+        }
+
+    }
+    else
+    {
+        Console.WriteLine($"El dato ingresado: {numberString}, no es un numero entero.");
+
+    }
+
+
+} while (numberString!.ToLower() != "salir");
+Console.WriteLine("Game Over.");
