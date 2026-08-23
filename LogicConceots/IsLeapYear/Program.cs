265b031b660e66693bbd6e2073c32c695da50cc1
María Camila Moreno <124942762+mcmorenou@users.noreply.github.com>
2026-08-22 23:35:18 -0500

    Is leap year

---
+++ LogicConceots/IsLeapYear/Program.cs
@@ -0,0 +1,60 @@
+﻿using Shared;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+
+do
+{
+    var currentYear = DateTime.Now.Year;
+    var message = string.Empty;
+    var year = ConsoleExtension.GetInt("Ingrese el año: ");
+
+    if (year == currentYear)
+    {
+        message = "es";
+    }
+
+    else if(year > currentYear)
+            {
+        message = "va a ser";
+            }
+    else
+    {
+        message = "fue";
+    }
+    if (year % 4 == 0)
+    {
+        if (year % 100 == 0)
+        {
+            if (year % 400 == 0)
+            {
+                Console.WriteLine($" El año: {year}, SI {message} biciesto.");
+            }
+            else
+            {
+                Console.WriteLine($" El año: {year}, NO {message} biciesto.");
+            }
+        }
+
+        else
+            {
+                Console.WriteLine($" El año: {year}, SI {message} biciesto.");
+            }
+        }
+
+        else
+            {
+                Console.WriteLine($" El año: {year}, NO {message} biciesto");
+            }
+
+   
+
+        do
+        {
+            answer = ConsoleExtension.GetValidOptions("¿ Deseas continuar [S]i, [N]o?: ", options);
+        } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+
+
+
+    } while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase)) ;
+    Console.WriteLine("Game Over.");
