78993b70a93245fac916dfbe0be7eabc18201570
María Camila Moreno <124942762+mcmorenou@users.noreply.github.com>
2026-09-06 14:25:09 -0500

    Matrix

---
+++ LogicConceots/Matrix/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Matrix
+{
+    public class Matrix
+    {
+        public static int
+    }
+}
---
+++ LogicConceots/PalindromePhrases/Program.cs
@@ -0,0 +1,40 @@
+﻿using Shared;
+
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+
+do
+{
+    //Data input
+    Console.BackgroundColor = ConsoleColor.Blue;
+    Console.Clear();
+    Console.WriteLine("***FRACES PALINDROMAS***");
+    var phrase = ConsoleExtension.GetString("Ingrese la palabra o frase ");
+
+    //Do process
+    var isPalindrome = IsPalindrome(phrase);
+
+
+    //Show results
+    Console.BackgroundColor = ConsoleColor.Black;
+    Console.BackgroundColor = ConsoleColor.Yellow;
+
+    Console.BackgroundColor = ConsoleColor.Blue;
+    Console.BackgroundColor = ConsoleColor.White;
+
+    do
+    { 
+    answer = ConsoleExtension.GetValidOptions("¿ Deseas continuar [S]i, [N]o?: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+
+bool IsPalindrome(string? phrase)
+{
+    throw new NotImplementedException();
+}
+
+Console.WriteLine("Game Over.");
+
+
+
